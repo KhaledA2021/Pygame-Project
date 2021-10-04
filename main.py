d92b020179d00f1fd1a5913620df41f4e9e898a2
KhaledA2021 <ka42801981@gmail.com>
2021-10-04 19:35:03 -0400

    added sound file, made motion throughout the air

--- main.py
+++ main.py
@@ -1,22 +1,78 @@
 import pygame
 
+pygame.mixer.init()
 WIN = pygame.display.set_mode((800, 600))
 BACKGROUNDCOLOR = [0,0,0]
 WIN.fill(BACKGROUNDCOLOR)
 pygame.display.set_caption("Free Candy")
 FPS = 60
 
-def moveplayer(keys):
+
+
+def track_player_buttons(keys):
     if keys[pygame.K_UP] or keys[pygame.K_w]:
-        return 0, 10
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            if keys[pygame.K_x]:
+                return 2.5, -2.5
+            else:
+                return 5, -5
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            if keys[pygame.K_x]:
+                return -2.5, -2.5
+            else:
+                return -5, -5
+        else:
+            if keys[pygame.K_x]:
+                return 0, -2.5
+            else:
+                return 0, -5
+    elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            if keys[pygame.K_x]:
+                return 2.5, 2.5
+            else:
+                return 5, 5
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            if keys[pygame.K_x]:
+                return -2.5, 2.5
+            else:
+                return -5, 5
+        else:
+            if keys[pygame.K_x]:
+                return 0, 2.5
+            else:
+                return 0, 5
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        if keys[pygame.K_x]:
+            return 2.5, 0
+        else:
+            return 5, 0
+    elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        if keys[pygame.K_x]:
+            return -2.5, 0
+        else:
+            return -5, 0
     else:
         return 0, 0
 
 
-def main():
-    Playersprite = pygame.image.load("Heart Placeholder.PNG")
 
-    WIN.blit(Playersprite, (800,600))
+
+
+def move_player(x, y, current_pos):
+    current_pos = (current_pos[0] + x, current_pos[1] + y)
+    return current_pos
+
+
+def main():
+#    background_music = pygame.mixer.music.load("BIG_SHOT.mp3")
+    Playersprite = pygame.image.load("Heart_Placeholder.PNG")
+    Playersprite = pygame.transform.scale(Playersprite, (20, 20))
+    current_pos = (400, 300)
+    WIN.blit(Playersprite, current_pos)
+#    pygame.mixer.music.play(1)
+
+
     running = True
     frames = pygame.time.Clock()
 
@@ -28,11 +84,14 @@ def main():
         x_change = 0
         y_change = 0
 
-        WIN.fill(BACKGROUNDCOLOR)
-        WIN.blit(Playersprite, (400, 300))
-
         KEYS = pygame.key.get_pressed()
-        x_change , y_change = moveplayer(KEYS)
+        x_change , y_change = track_player_buttons(KEYS)
+        current_pos = move_player(x_change, y_change, current_pos)
+
+        WIN.fill(BACKGROUNDCOLOR)
+        WIN.blit(Playersprite, current_pos)
+
+
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
                 running = False
