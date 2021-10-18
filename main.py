07f048b25c84febed445ebe64fcce1640163545f
KhaledA2021 <ka42801981@gmail.com>
2021-10-18 19:34:43 -0400

    added banger song

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pygame
 
 pygame.mixer.init()
 WIN = pygame.display.set_mode((800, 600))
-BACKGROUNDCOLOR = [0,0,0]
+BACKGROUNDCOLOR = [10,0,24]
 WIN.fill(BACKGROUNDCOLOR)
 pygame.display.set_caption("Free Candy")
 FPS = 60
 
-
+MOOSIC = pygame.mixer.Sound("Stratales - Spaghetti On The Side.WAV")
+MOOSIC.play(-1)
 
 def track_player_buttons(keys):
     if keys[pygame.K_UP] or keys[pygame.K_w]:
@@ -58,36 +59,32 @@ def track_player_buttons(keys):
 
 def move_player(x, y, current_pos):
     current_pos = (current_pos[0] + x, current_pos[1] + y)
-    if current_pos[0] == 0:
-        current_pos = (current_pos[0] + 5, current_pos[1])
-    if current_pos[0] == 800:
-        current_pos = (current_pos[0] + -5, current_pos[1])
-    if current_pos[1] == 0:
-        current_pos = (current_pos[0], current_pos[1] + 5)
-    if current_pos[1] == 600:
-        current_pos = (current_pos[0], current_pos[1] + -5)
+    if current_pos[0] < -20:
+        current_pos = (-20, current_pos[1])
+    if current_pos[0] > 800:
+        current_pos = (800, current_pos[1])
+    if current_pos[1] < -20:
+        current_pos = (current_pos[0], -20)
+    if current_pos[1] > 600:
+        current_pos = (current_pos[0], 600)
     return current_pos
 
-
-def rotate(Rotation, Direction):
-    Rotation =+ Direction
-    return Rotation
 
 
 def main():
 #    background_music = pygame.mixer.music.load("BIG_SHOT.mp3")
     Playersprite = pygame.image.load("ProjectPSprite.PNG")
-    Playersprite = pygame.transform.scale(Playersprite, (20, 20))
+    Playersprite = pygame.transform.scale(Playersprite, (10, 10))
     current_pos = (400, 300)
     Rotation = 0
     WIN.blit(Playersprite, current_pos)
-#    pygame.mixer.music.play(1)
+
 
 
     running = True
     frames = pygame.time.Clock()
 
-
+    wait_time = 0
     while running:
         frames.tick(FPS)
         print(frames)
@@ -97,7 +94,16 @@ def main():
 
         KEYS = pygame.key.get_pressed()
         x_change , y_change = track_player_buttons(KEYS)
+        if KEYS[pygame.K_SPACE]:
+            if wait_time == 0:
+                x_change = x_change * 2
+                y_change = y_change * 2
+                wait_time = 30
+
         current_pos = move_player(x_change, y_change, current_pos)
+
+        if wait_time > 0:
+            wait_time =- 1
 
 
         WIN.fill(BACKGROUNDCOLOR)
