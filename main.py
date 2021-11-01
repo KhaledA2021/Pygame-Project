73e7cf8e1b08f36f81a1ea408de98023c9d49ea2
KhaledA2021 <ka42801981@gmail.com>
2021-11-01 19:35:50 -0400

    ooga booga

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.mixer.init()
 WIN = pygame.display.set_mode((800, 600))
@@ -10,71 +11,81 @@ FPS = 60
 MOOSIC = pygame.mixer.Sound("Creatures-Ov-Deception-_Jsab-Instrumental-Edit_.WAV")
 MOOSIC.play(-1)
 
-def track_player_buttons(keys):
+timer = time.time()
+
+def track_player_buttons(keys, rotation):
     if keys[pygame.K_UP] or keys[pygame.K_w]:
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             if keys[pygame.K_x]:
-                return 2.5, -2.5
+                return 2.5, -2.5, 45
             else:
-                return 5, -5
+                return 5, -5, 45
         elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
             if keys[pygame.K_x]:
-                return -2.5, -2.5
+                return -2.5, -2.5, 315
             else:
-                return -5, -5
+                return -5, -5, 315
         else:
             if keys[pygame.K_x]:
-                return 0, -2.5
+                return 0, -2.5, 0
             else:
-                return 0, -5
+                return 0, -5, 0
     elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             if keys[pygame.K_x]:
-                return 2.5, 2.5
+                return 2.5, 2.5, 135
             else:
-                return 5, 5
+                return 5, 5, 135
         elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
             if keys[pygame.K_x]:
-                return -2.5, 2.5
+                return -2.5, 2.5, 225
             else:
-                return -5, 5
+                return -5, 5, 225
         else:
             if keys[pygame.K_x]:
-                return 0, 2.5
+                return 0, 2.5, 180
             else:
-                return 0, 5
+                return 0, 5, 180
     elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         if keys[pygame.K_x]:
-            return 2.5, 0
+            return 2.5, 0, 90
         else:
-            return 5, 0
+            return 5, 0, 90
     elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
         if keys[pygame.K_x]:
-            return -2.5, 0
+            return -2.5, 0, 270
         else:
-            return -5, 0
+            return -5, 0, 270
     else:
-        return 0, 0
+        return 0, 0, rotation
 
 
 def move_player(x, y, current_pos):
     current_pos = (current_pos[0] + x, current_pos[1] + y)
-    if current_pos[0] < -20:
-        current_pos = (-20, current_pos[1])
-    if current_pos[0] > 800:
-        current_pos = (800, current_pos[1])
-    if current_pos[1] < -20:
-        current_pos = (current_pos[0], -20)
-    if current_pos[1] > 600:
-        current_pos = (current_pos[0], 600)
+    if current_pos[0] < -10:
+        current_pos = (-10, current_pos[1])
+    if current_pos[0] > 790:
+        current_pos = (790, current_pos[1])
+    if current_pos[1] < -10:
+        current_pos = (current_pos[0], -10)
+    if current_pos[1] > 590:
+        current_pos = (current_pos[0], 590)
     return current_pos
 
-
+def redraw_screen(BACKGROUNDCOLOR, Playersprite, Rotation, current_pos, Playerhealth, health_rotate, health_pos):
+    WIN.fill(BACKGROUNDCOLOR)
+    Playersprite = pygame.transform.rotate(Playersprite, Rotation)
+    Playerhealth = pygame.transform.rotate(Playerhealth, health_rotate)
+    WIN.blit(Playerhealth, health_pos)
+    WIN.blit(Playersprite, current_pos)
 
 def main():
 #    background_music = pygame.mixer.music.load("BIG_SHOT.mp3")
     Playersprite = pygame.image.load("ProjectPSprite.PNG")
+    Playerhealth = pygame.image.load("ProjectPSpriteHealth.PNG")
+
     Playersprite = pygame.transform.scale(Playersprite, (15, 15))
+    Playerhealth = pygame.transform.scale(Playerhealth, (10, 10))
     current_pos = (400, 300)
     Rotation = 0
     WIN.blit(Playersprite, current_pos)
@@ -87,18 +98,21 @@ def main():
     wait_time = 0
     while running:
         frames.tick(FPS)
-        print(frames)
+        print(frames, timer)
 
         x_change = 0
         y_change = 0
 
+        health_pos = (current_pos[0]+2.5, current_pos[1]+2.5)
+        health_rotate = Rotation
+
         KEYS = pygame.key.get_pressed()
-        x_change , y_change = track_player_buttons(KEYS)
+        x_change , y_change, Rotation = track_player_buttons(KEYS, Rotation)
         if KEYS[pygame.K_SPACE]:
-            if wait_time == 0:
-                x_change = x_change * 2
-                y_change = y_change * 2
+            if wait_time < 1:
+                current_pos = (current_pos[0]+(x_change*3), current_pos[1]+(y_change*3))
                 wait_time = 30
+
 
         current_pos = move_player(x_change, y_change, current_pos)
 
@@ -106,9 +120,7 @@ def main():
             wait_time =- 1
 
 
-        WIN.fill(BACKGROUNDCOLOR)
-        Playersprite = pygame.transform.rotate(Playersprite, Rotation)
-        WIN.blit(Playersprite, current_pos)
+        redraw_screen(BACKGROUNDCOLOR, Playersprite, Rotation, current_pos, Playerhealth, health_rotate, health_pos)
 
 
 
