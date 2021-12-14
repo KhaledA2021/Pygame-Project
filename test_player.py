d98bab12b4853b909f4188fde2a08461b9a8029e
KhaledA2021 <ka42801981@gmail.com>
2021-12-13 19:39:28 -0500

    fixing timer

--- test_player.py
+++ test_player.py
@@ -4,6 +4,7 @@ import pygame
 
 class player:
 
+    timer = 0
 
     def __init__(self):
         self.reset_player()
@@ -24,7 +25,7 @@ class player:
         self.rotate = 0
         self.hitbox = True
         self.health = 3
-        self.Player_Sprite = pygame.image.load("ProjectPSprite.PNG")
+        self.Player_Sprite = pygame.image.load("Sprites/ProjectPSprite.PNG")
         self.Player_Sprite = pygame.transform.scale(self.Player_Sprite, (15, 15))
 
 
@@ -33,42 +34,58 @@ class player:
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 if keys[pygame.K_x]:
                     self.update_values(2.5, -2.5, 45)
+                elif keys[pygame.K_SPACE]:
+                    self.update_values(15, -15, 45)
                 else:
                     self.update_values(5, -5, 45)
             elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
                 if keys[pygame.K_x]:
                     self.update_values(-2.5, -2.5, 315)
+                elif keys[pygame.K_SPACE]:
+                    self.update_values(-15, -15, 315)
                 else:
                     self.update_values(-5, -5, 315)
             else:
                 if keys[pygame.K_x]:
                     self.update_values(0, -2.5, 0)
+                elif keys[pygame.K_SPACE]:
+                    self.update_values(0, -15, 0)
                 else:
                     self.update_values(0, -5, 0)
         elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 if keys[pygame.K_x]:
                     self.update_values(2.5, 2.5, 135)
+                elif keys[pygame.K_SPACE]:
+                    self.update_values(15, 15, 135)
                 else:
                     self.update_values(5, 5, 135)
             elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
                 if keys[pygame.K_x]:
                     self.update_values(-2.5, 2.5, 225)
+                elif keys[pygame.K_SPACE]:
+                    self.update_values(-15, 15, 225)
                 else:
                     self.update_values(-5, 5, 225)
             else:
                 if keys[pygame.K_x]:
                     self.update_values(0, 2.5, 180)
+                elif keys[pygame.K_SPACE]:
+                    self.update_values(0, 15, 180)
                 else:
                     self.update_values(0, 5, 180)
         elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             if keys[pygame.K_x]:
                 self.update_values(2.5, 0, 90)
+            elif keys[pygame.K_SPACE]:
+                self.update_values(15, 0, 90)
             else:
                 self.update_values(5, 0, 90)
         elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
             if keys[pygame.K_x]:
                 self.update_values(-2.5, 0, 270)
+            elif keys[pygame.K_SPACE]:
+                self.update_values(-15, 0, 270)
             else:
                 self.update_values(-5, 0, 270)
         else:
@@ -87,4 +104,13 @@ class player:
         elif self.pos[1] > 585:
             self.pos = (self.pos[0], 585)
         self.rotate = rotation
+
+
+    def timer(self, timer_set_time, set_timer, timer):
+        if set_timer == True:
+            timer = timer_set_time
+        elif set_timer == False and timer != 0:
+            timer -= timer
+            print(timer)
+
 
